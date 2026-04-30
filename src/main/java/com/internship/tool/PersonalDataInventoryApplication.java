cfcafe9585a3aa8630a2ca3b48d975a24bdfdf4f
Yashas <yashas@gmail.com>
2026-04-30 19:56:46 +0530

    Day 9 completed: Added logging aspect, API monitoring, and auditing timestamps

--- src/main/java/com/internship/tool/PersonalDataInventoryApplication.java
+++ src/main/java/com/internship/tool/PersonalDataInventoryApplication.java
@@ -3,8 +3,10 @@ package com.internship.tool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
+@EnableJpaAuditing   // 🔥 VERY IMPORTANT (for createdAt & updatedAt)
 public class PersonalDataInventoryApplication {
 
     public static void main(String[] args) {
